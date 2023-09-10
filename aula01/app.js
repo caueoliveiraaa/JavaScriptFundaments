4d0c6d2c9ccb3d3ad1dcc3f4727a2c767610a705
caueoliveiraaa <leo.siscobra@gmail.com>
2023-09-10 15:14:06 -0300

    update

---
+++ aula01/app.js
@@ -0,0 +1,12 @@
+// using alerts
+// alert('hello')
+
+// display values in console on browser
+// console.log(1)
+// console.log(2)
+
+// variables
+let age = 31
+let current_year = 2023
+
+console.log(age, current_year)
