877a56938a97024898da301d59d79c67102e9609
caueoliveiraaa <leo.siscobra@gmail.com>
2024-04-21 14:51:54 -0300

    website
    feat: Setting data of html elements via JavaScript.

---
+++ website/index.js
@@ -0,0 +1,14 @@
+console.log('website is running');
+
+//window.alert('Alert')
+//window.alert('Alert 2')
+
+// Comment 
+
+/* 
+Many
+Comments
+*/
+
+document.getElementById('myH1').textContent = 'This is a header';
+document.getElementById('myP').textContent = 'This is a paragraph';
